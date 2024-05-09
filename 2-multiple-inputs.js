32cd2e9e1708f648a30e777f455726ceae097a73
Roggers Anguzu <141458053+roggersanguzu@users.noreply.github.com>
2024-05-09 22:10:57 +0300

    Update 2-multiple-inputs.js

--- 2-multiple-inputs.js
+++ 2-multiple-inputs.js
@@ -1,10 +1,4 @@
 import React, { useState } from 'react';
-// JS
-// const input = document.getElementById('myText');
-// const inputValue = input.value
-// React
-// value, onChange
-// dynamic object keys
 
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
